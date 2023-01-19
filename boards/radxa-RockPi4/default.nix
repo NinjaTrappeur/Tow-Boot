b98e3935d3428d069b1d8aea59f3fdae0cf440f1
Félix Baylac Jacqué <felix@alternativebit.fr>
2023-01-19 15:11:09 +0100

    debug

--- boards/radxa-RockPi4/default.nix
+++ boards/radxa-RockPi4/default.nix
@@ -19,11 +19,14 @@
       (helpers: with helpers; {
         USE_PREBOOT = yes;
         PREBOOT = freeform ''"usb start"'';
+        LOG = yes;
+        LOG_MAX_LEVEL = freeform "9";
       })
     ];
     patches = [
       # Based on https://github.com/armbian/build/blob/master/patch/u-boot/u-boot-rockchip64/board-rock-pi-4-enable-spi-flash.patch
       ./0001-rockpi4-rk3399-add-spi-support.patch
+      ./0001-Towboot-debug-heh-did-not-find-a-better-way-to-debug.patch
       # From https://github.com/armbian/build/blob/master/patch/u-boot/u-boot-rockchip64/general-add-xtx-spi-nor-chips.patch
       ./general-add-xtx-spi-nor-chips.patch
     ];
